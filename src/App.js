ff17148e153400c908598f31eb21b5bee21cda88
fazal <fazalahamed628@gmail.com>
2024-04-29 14:35:57 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState('');
+  const [checked,setChecked]=useState([])
+
+  
+  const handleDeleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+      </div>
+      <div className="input">
+        <input value={todo} onChange={(e) => setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => setTodos([...todos, { id: Date.now(), text: todo, status: false }])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {todos.map((obj) => (
+          <div className="todo" key={obj.id}>
+            <div className="left">
+              <input onChange={(e) => {
+                setTodos(todos.map(todoItem => {
+                  if (todoItem.id === obj.id) {
+                    
+                    return { ...todoItem, status: e.target.checked };
+                  }
+                  return todoItem;
+                }));
+              }} value={obj.status} type="checkbox" name="" id="" />
+              <p>{obj.text}</p>
+            </div>
+            <div className="right">
+              <i onClick={() => handleDeleteTodo(obj.id)} className="fas fa-times"></i>
+            </div>
+          </div>
+        ))}
+        {todos.map((obj) => {
+          if (obj.status) {
+            return <h1 key={obj.id}>{obj.text}</h1>;
+          }
+          return null;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/Checked.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function checked() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default checked
